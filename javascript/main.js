b8c5c7290d2aafc08797fda7c732c1663dcb013b
twotl <tobias.awotula@outlook.de>
2020-03-23 17:49:17 +0100

    add % operator, add negative signed numbers

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,122 @@
-import {evaluateInput} from './evaluate.js';
-import { evaluate } from "./evaluate";
 
+// DOM elements
 const container = document.getElementById('container');
-
-const inputBtns = [...container.getElementsByTagName('button')].filter(btn => {
-    return (/^[+\d-*/.()=]$/).test(btn.textContent);
-});
-
-let [cache, input] = [...document.getElementById('screen').getElementsByTagName('*')]
-const calculator = {input: []}
-
+const inputBtns = [...container.getElementsByTagName('button')];
+const screen = document.getElementById('screen')
+let [cache, input] = [...screen.getElementsByTagName('*')]
 inputBtns.forEach(btn=>{
     btn.addEventListener('click', () => handleInput(btn.textContent))
 })
 
+wildCardBtn = document.getElementById('smile');
+wildCardBtn.addEventListener('click' ,() => {
+    const color1 = Math.floor(Math.random() * 256**3);
+    const color2 = Math.floor(Math.random() * 256**3);
+    screen.style.color = `#${color1.toString(16)}`;
+    screen.style.backgroundColor = `#${color2.toString(16)}c9`;
+});
+document.addEventListener('keydown', (e) => handleInput(e.key));
+
+const calculator = {input: []}
+
+
+function updateCache() {
+    cache.textContent = calculator.input.join('');
+}
+
+function clearScreen(){
+    input.textContent = '';
+}
+
+function clearInput(){
+    calculator.input = [];
+}
+
+function clearAll() {
+    clearInput();
+    clearScreen();
+    updateCache()
+}
+
+
+const inputFlags = {
+    decimal: false, 
+    operator: false,
+    newInput: false,
+    bracketsOpen: 0, 
+}
+const isOperator = key => (/^[\+|-|\*|\/|%]$/).test(key);
+const isNumber = key => (/^-?\d*[.]?\d+$/).test(key);
+
+function handleInput(key){
+    let currentText = input.textContent;
+    if((/^[\d]$/).test(key)) {
+        if(isOperator(currentText) && currentText != '-' ){
+                calculator.input.push(currentText)
+                updateCache();
+                clearScreen();
+                inputFlags['decimal'] = false;
+        }
+        input.textContent += key;
+    } else if (isOperator(key)) {
+        
+        if (isNumber(currentText)){
+            calculator.input.push(+currentText);
+            updateCache();
+            input.textContent = key;
+        } else if(currentText === '' && key === '-'){
+            input.textContent += key;
+        } 
+        
+    } else if((/[,.]/).test(key)) {
+        input.textContent += (!inputFlags.decimal)? '.' : '';
+        inputFlags['decimal'] = true;
+    } else {
+        switch (key) {
+               
+            case '=':
+            case 'Enter':
+                if(isNumber(currentText)){
+                    calculator.input.push(+currentText);
+                    clearScreen()
+                    updateCache()
+                    cache.textContent += ' = ';
+                    let output = evaluateInput(calculator.input);
+                    const decimals = 1000000;
+                    input.textContent = (isNaN(output))?output:Math.round(output * decimals) / decimals ;
+                    calculator['input'] = [];
+                }
+                break;
+    
+            case 'Backspace':
+            case 'DEL':
+                
+                if(currentText.length > 1){
+                    input.textContent = currentText.slice(0, currentText.length-1);
+                    
+                } else {
+                    clearScreen();
+                    input.textContent = (0 != calculator.input.length )? 
+                        calculator.input.pop() : input.textContent;
+                    updateCache()
+                }
+                break;
+            case 'Delete':
+            case 'C':    
+                clearAll()
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
+
+
 function operate(op, num1, num2) {
     let result;
-
+    
     switch (op) {
         case '+':
             result = num1 + num2;
@@ -28,7 +128,12 @@ function operate(op, num1, num2) {
             result = num1 * num2;
             break;
         case '/':
-            result = (num1 === 0 && num2 === 0 )?'Not defined yet': num1 / num2;
+            const zeroDividedBy0ErrText = ' 0/0 is Undefined';
+            result = (num1 === 0 && num2 === 0 )? zeroDividedBy0ErrText: num1 / num2;
+            break;
+        
+        case '%':
+            result = num1 % num2;
             break;
         
         default:
@@ -38,91 +143,45 @@ function operate(op, num1, num2) {
     return result;
 }
 
-function updateCache() {
-    cache.textContent = calculator.input.join('');
-}
-
-function clearScreen(){
-    input.textContent = ''
-}
-
-function clearInput(){
-    calculator.input = [];
-}
-
-function clearAll() {
-    clearInput();
-    clearScreen();
-}
-
-
-const inputFlags = {decimal: false, bracketsOpen: 0, operator: false,}
-const isOperator = key => (/\+|-|\*|\//).test(key);
-const isNumber = key => (/^\d*[.]?\d+$/).test(key);
-
-function handleInput(key){
-    console.log(key);
-    const currentText = input.textContent;
-    if((/[\d]/).test(key)) {
-        if(isOperator(currentText)){
-            calculator.input.push(currentText)
-            updateCache();
-            clearScreen();
-            inputFlags['decimal'] = false;
-        }
-        input.textContent += key;
-    } else if (isOperator(key)) {
-        // is Number? => push
-        if (isNumber(currentText)){
-            
-            calculator.input.push(currentText);
-            updateCache();
-            input.textContent = key;
-        } else if (isOperator) {
-            input.textContent = key;
-            
-        }
-    } else if((/[,.]/).test(key)) {
-        console.log('period');
-        input.textContent += (!inputFlags.decimal)? key : '';
-        inputFlags['decimal'] = true;
-    } else {
-        switch (key) {
-               
-            case 'Enter':
-            case '=':
-                if(isNumber(currentText) ){
-                    calculator.input.push(input.textContent);
-                    clearScreen()
-                    updateCache()
-                    cache.textContent += ' = ';}
-                    input.textContent = evaluateInput(calculator.input)
-                break;
-    
-            case 'Backspace':
-            case 'DEL':
-                
-                if(currentText.length > 0){
-                    input.textContent = currentText.slice(0, text.length-1);
-                     currenText = input.textContent;
-                     input.textContent = (currentText=='' && 0 != calculator.input.length )? 
-                        calculator.input.pop() : input.textContent;
-                        updateCache()
-                    }
-                break;
-            case 'Delete':
-            case 'C':    
-                clearAll()
-                break;
-            
-            case 'CE':    
-                clearScreen()
-                break;
-        
-            default:
-                break;
-        }
+function evaluateInput(input) {
+    console.log(input);
+    if(input.length ===1) {
+        // parenthesized expression?
+        if( (/\(/).test(input[0]) ){
+            // remove outer brackets then evaluate
+            return evaluateInput(input[0].slice(1,input.length-1))
+        } 
+        return input[0];
     }
-}
+    
+    const lastOperator = input[input.length-2];
+    const index = input.lastIndexOf(lastOperator);
+    let operator;
+    let left, right;
+    
+    // split input into it's binary expressions 
+    // in such a way that it's evaluate left to right
+    switch (lastOperator) {
+        case '+':
+        case '-': 
+            operator = lastOperator;
+            left = input.slice(0, index);
+            right = input.slice(index+1);
+            break;
+        
+        case '*':
+        case '/': 
+        case '%': 
+            operator = lastOperator;
+            left = input.slice(0, index);
+            right = input.slice(index+1);
 
-document.addEventListener('keydown', (e) => handleInput(e.key));
+            break;
+     
+        default:
+            console.log('not an operator: ' + lastOperator);
+            break;
+    }
+    console.log(operate(lastOperator, evaluateInput(left), evaluateInput(right)));
+    return operate(lastOperator, evaluateInput(left), evaluateInput(right));
+}
